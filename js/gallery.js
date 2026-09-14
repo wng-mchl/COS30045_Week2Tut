fcae88017ce0300329f754834ee99048e6ed9fac
wng-mchl <maxmicwong@gmail.com>
2026-09-15 01:17:25 +0800

    feat: added title element to chart display, requires styling

--- js/gallery.js
+++ js/gallery.js
@@ -1,31 +1,34 @@
 (function () {
-const charts = [
-    { src: "../images/Q1_BarChart.png", placeholder: ""},
-    { src: "../images/Q1_PieChart.png", placeholder: ""},
-    { src: "../images/Q2_BarChart.png", placeholder: ""},
-    { src: "../images/Q3_BarChart.png", placeholder: ""},
-    { src: "../images/Q4_BarChart.png", placeholder: ""},
-    { src: "../images/Q5_ScatterPlot.png", placeholder: ""},
-    { src: "../images/Q6_ScatterPlot.png", placeholder: ""},
-    { src: "../images/Q7_BarChart.png", placeholder: ""},
-    { src: "../images/Q7_BoxPlot.png", placeholder: ""},
-];
+    const charts = [
+        { src: "../images/Q1_BarChart.png", title: "", placeholder: "" },
+        { src: "../images/Q1_PieChart.png", title: "", placeholder: "" },
+        { src: "../images/Q2_BarChart.png", title: "", placeholder: "" },
+        { src: "../images/Q3_BarChart.png", title: "", placeholder: "" },
+        { src: "../images/Q4_BarChart.png", title: "", placeholder: "" },
+        { src: "../images/Q5_ScatterPlot.png", title: "", placeholder: "" },
+        { src: "../images/Q6_ScatterPlot.png", title: "", placeholder: "" },
+        { src: "../images/Q7_BarChart.png", title: "", placeholder: "" },
+        { src: "../images/Q7_BoxPlot.png", title: "", placeholder: "" },
+    ];
 
-const constainer = document.getElementById("gallery");
+    const container = document.getElementById("gallery");
 
-charts.forEach((chart) => {
-    const figure = document.createElement("figure");
-    figure.className ="chart-card";
+    charts.forEach((chart) => {
+        const figure = document.createElement("figure");
+        figure.className = "chart-card";
 
-    const img = document.createElement("img");
-    img.src = chart.src
-    img.placeholder = chart.placeholder;
+        const title = document.createElement("h3");
+        title.textContent = chart.title;
+        const img = document.createElement("img");
+        img.src = chart.src
+        img.placeholder = chart.placeholder;
 
-    const figcaption = document.createElement("figurecaption");
-    figcaption.textContent = chart.caption;
+        const figcaption = document.createElement("figurecaption");
+        figcaption.textContent = chart.caption;
 
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    constainer.appendChild(figure);
-})
+        figure.appendChild(title);
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        container.appendChild(figure);
+    })
 })();
